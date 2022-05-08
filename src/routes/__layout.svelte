38763e65cfe47447008645b0826dd5e819f3e3f5
Roy <roy@osherove.com>
2022-05-08 12:27:32 +0300

    meta tags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,31 @@
 <script>
-    import "../app.css";
+	import '../app.css';
+	import { MetaTags } from 'svelte-meta-tags';
 </script>
 
+<head>
+	<MetaTags
+		title="קהילת פיתוח ווב3 בישראל"
+		titleTemplate="קהילת פיתוח ווב3 בישראל"
+		description="דף הבית של קהילת פיתוח ווב3 בישראל - מפגשים דיסקורד טלגרם ועוד"
+		canonical="https://www.israelweb3.com"
+		openGraph={{
+			url: 'https://www.israelweb3.com',
+			title: 'Afikoman 2022 Israel web3',
+			description: 'הגרלת האפיקומן של ווב3',
+			images: [
+				{
+					url: 'https://www.israelweb3.com/bored.png',
+					width: 800,
+					height: 600,
+					alt: 'Israel Web3'
+				}
+			],
+			site_name: 'קהילת פיתוח ווב3 בישראל'
+		}}
+	/>
+</head>
+
 <div class="mt-10 md:mt-20">
-    <slot />
+	<slot />
 </div>
