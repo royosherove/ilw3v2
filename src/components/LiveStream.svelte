c197c73d71639ae64578c189c3cf0bf1653eb2c3
Roy <roy@osherove.com>
2022-05-09 22:21:52 +0300

    fix live stream maybe

--- src/components/LiveStream.svelte
+++ src/components/LiveStream.svelte
@@ -1,13 +1,11 @@
-	<div class="mt-10 text-xl text-white">שידור חי</div>
-	<div style="padding:56.25% 0 0 0;position:relative;" class="mt-1 mx-2 ">
-		<iframe
-			id="r-embed-player-iframe"
-			class="rounded"
-			src="https://player.restream.io/?token=c2b08d5f97664a3dbb1378ceba801a1e"
-			allow="autoplay"
-			allowfullscreen
-			frameborder="0"
-			title="Web3 Israel Stream"
-			style="position:absolute;top:0;left:0;width:100%;height:100%;"
-		/>
-	</div>
+<div class="mt-10 text-xl text-white">שידור חי</div>
+<div style="padding:56.25% 0 0 0;position:relative;">
+	<iframe
+		src="https://player.restream.io/?token=c2b08d5f97664a3dbb1378ceba801a1e"
+		allow="autoplay"
+		allowfullscreen
+		frameborder="0"
+		style="position:absolute;top:0;left:0;width:100%;height:100%;"
+		title="Live Stream"
+	/>
+</div>
