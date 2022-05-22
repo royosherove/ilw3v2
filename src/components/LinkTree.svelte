3c3b27d9eb84f708a343bc8d18f926b0cf771c6e
Roy <roy@osherove.com>
2022-05-22 14:09:50 +0300

    podcase and links json file

--- src/components/LinkTree.svelte
+++ src/components/LinkTree.svelte
@@ -1,41 +1,13 @@
 <script>
 	// import EventsCalendar from '../src/components/eventsCalendar.svelte';
 	import { each } from 'svelte/internal';
-
-	const links = [
-		{ name: 'דיסקורד (פיתוחי)', url: 'https://discord.gg/YnpgKfcyyW', image: 'discord.png' },
-		{ name: 'טלגרם (פיתוחי)', url: 'https://t.me/+-sEflgfrqDoyYWVk', image: 'telegram.png' },
-		{
-			name: 'מיטאפ (פיתוחי)',
-			url: 'https://www.meetup.com/ilw3-israel-web-3-0-smart-contracts-dev-meetup/',
-			image: 'meetup.png'
-		},
-		{
-			name: 'YouTube (פיתוחי)',
-			url: 'https://youtube.com/channel/UCRfgLdabcXDFKhv_BFazjYw',
-			image: 'youtube.png'
-		},
-		{
-			name: 'ווב3 בפייסבוק (פיתוחי)',
-			url: 'https://www.facebook.com/groups/web3.israel',
-			image: 'fb.png'
-		},
-		{
-			name: 'מטא בילדרס (פיתוחי)',
-			url: 'https://www.facebook.com/groups/metabuilders',
-			image: 'fb.png'
-		}
-	];
-	const otherLinks = [
-		{ name: 'Web3il (כללי)', url: 'https://discord.gg/2qTfa2HKWn', image: 'discord.png' },
-		{ name: 'Web3il טלגרם (כללי)', url: 'https://t.me/Web3Hebrew', image: 'telegram.png' }
-	];
+	import * as allLinks from '../links.json' ;
 </script>
 
 <div class="md:flex md:space-x-12 md:m-auto">
 	<div>
 		<div class="text-white font-mono mt-10">לינקים בנושא פיתוח</div>
-		{#each links as link}
+		{#each allLinks.devlinks as link}
 			<a href={link.url} class="">
 				<div
 					class="border-black w-3/4 md:w-full my-2 m-auto bg-gray-50 border rounded p-2 md:p-3 flex items-center "
@@ -50,7 +22,7 @@
 	</div>
 	<div>
 		<div class="text-white font-mono mt-10">לינקים כלליים</div>
-		{#each otherLinks as link}
+		{#each allLinks.generalLinks as link}
 			<a href={link.url} class="">
 				<div
 					class="border-black w-3/4 md:w-full my-2 m-auto bg-gray-50 border rounded p-2 md:p-3 flex items-center "
